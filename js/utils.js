17ccdf5d60dded1171578208dfe2d563eb9e1f02
d4vves <d4vves@gmail.com>
2023-09-08 14:37:55 -0700

    more level logic, added music

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,4 @@
-function advanceLevel() {
+function advanceLevel(physics) {
     messageText.destroy();
     gameAdvance = false;
     stageNumber ++;
@@ -6,22 +6,29 @@ function advanceLevel() {
     garbageBoy.body.reset(745, 285);
     rats.children.iterate(function (rat) {
         rat.body.reset(Phaser.Math.Between(130, 765), Phaser.Math.Between(125, 560));
-        rat.setVelocity(-95 - 5 - (stageNumber * 10), -75 - (stageNumber * 10));
-        let ratVelocity = -95 - 5 - (stageNumber * 10)
+        rat.setVelocity(ratMovementSpeed - (stageNumber * 10), ratMovementSpeed - (stageNumber * 10));
     });
+    physics.resume();
 }
 
 function collideCan(garbageBoy, rats) {
-    this.physics.pause();
-    messageText = this.add.text(400, 300, 'Welcome home, GB!', { fontSize: '32px', fill: '#C6CA53' });
-    gameAdvance = true;
+    if (hasTrashPickup) {
+        this.physics.pause();
+        messageText = this.add.text(400, 300, 'Welcome home, GB!', { fontSize: '32px', fill: '#C6CA53' });
+        gameAdvance = true;
+    } else {
+        gameMessage.setVisible(true);
+        setTimeout(() => {
+            gameMessage.setVisible(false);
+        }, 3000)
+    }
 }
 
 function collidePowerUps(garbageBoy, powerUp) {
     powerUp.disableBody(true, true);
+    hasTrashPickup = true;
     hasPowerUp = true;
     inventory = powerUp.body.gameObject.texture.key;
-    console.log('INVENTORY: ', inventory)
 }
 
 function collideRat(garbageBoy, rats) {
@@ -34,21 +41,22 @@ function randomPowerUp() {
     return Math.random() < 0.5 ? 'bottle' : 'candy';
 }
 
-function resetLevel() {
+function resetLevel(physics) {
     messageText.destroy();
     gameOver = false;
     garbageBoy.body.reset(745, 285);
     rats.children.iterate(function (rat) {
         rat.body.reset(Phaser.Math.Between(130, 765), Phaser.Math.Between(125, 560));
-        rat.setVelocity(-95, -95);
+        rat.setVelocity(ratMovementSpeed, ratMovementSpeed);
     });
+    physics.resume();
 }
 
-function spaceBarDown(gameAdvance, gameOver) {
+function spaceBarDown(gameAdvance, gameOver, physics) {
     if (gameAdvance) {
-        advanceLevel();
+        advanceLevel(physics);
     } else if (gameOver) {
-        resetLevel();
+        resetLevel(physics);
     } else if (hasPowerUp) {
         usePowerUp(inventory);
     }
